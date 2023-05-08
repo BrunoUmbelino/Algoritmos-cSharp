387b8815ce9f562f583e7c2009757663606da85b
Bruno Umbelino <bruno.umbelino0@gmail.com>
2023-05-07 23:31:59 -0300

    Adicionando Algoritimo de Busca Binária

---
+++ Algoritmos/Algoritmos/Busca.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmos
+{
+    internal class Busca
+    {
+        public int ContadorDeTentativas { get; set; } = 0;
+        public int? BuscaBinaria(List<int> list, int itemBuscado)
+        {
+            var _list = list;
+            var baixo = 0;
+            var alto = _list.Count - 1;
+
+            while(baixo <= alto)
+            {
+                ContadorDeTentativas++;
+
+                var meio = (baixo + alto) / 2;
+                var chute = Math.Floor((double)_list[meio]);
+                Console.WriteLine("chute: " + chute);
+
+                if (chute == itemBuscado)
+                    return itemBuscado;
+                if (chute > itemBuscado)
+                    alto = meio - 1;
+                if (chute < itemBuscado)
+                    baixo = meio + 1;
+            }
+
+            return null;
+        }
+    }
+}
--- Algoritmos/Algoritmos/Program.cs
+++ Algoritmos/Algoritmos/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Algoritmos;
+
+var lista = new List<int> { };
+for (var i = 1; i<= 100; i++)
+{
+    lista.Add(i);
+}
+
+Console.WriteLine("Digite o número que deseja buscar da lista: ");
+var item = Convert.ToInt32(Console.ReadLine());
+
+var listaConcatenada = String.Join(",", lista);
+Console.WriteLine($"\nLista: [{listaConcatenada}]\n");
+
+var busca = new Busca();
+var resultado = busca.BuscaBinaria(lista, item);
+
+if (resultado == null)
+{
+    Console.WriteLine($"\nNúmero {item} não encontrado."); 
+    return;
+}
+
+Console.WriteLine($"\nNúmero {resultado} encontrado com {busca.Contador} tentativas.");
+
+
