d0e3e1a50b42033feaf2226859f98ea25b6439e1
Bruno Umbelino <bruno.umbelino0@gmail.com>
2023-05-10 02:06:00 -0300

    Adicionando algoritmo parcial de OrdenacaoPorSelecao

---
+++ Algoritmos/Algoritmos/BuscaBinaria.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmos
+{
+    internal class BuscaBinaria
+    {
+        public int ContadorDeTentativas { get; set; } = 0;
+        public int? ImplementaBuscaBinaria(List<int> list, int itemBuscado)
+        {
+            var _list = list;
+            var baixo = 0;
+            var alto = _list.Count - 1;
+
+            while(baixo <= alto)
+            {
+                ContadorDeTentativas++;
+
+                var meio = (baixo + alto) / 2;
+                var chute = Math.Floor((double)_list[meio]);
+                Console.WriteLine("chute: " + chute);
+
+                if (chute == itemBuscado)
+                    return itemBuscado;
+                if (chute > itemBuscado)
+                    alto = meio - 1;
+                if (chute < itemBuscado)
+                    baixo = meio + 1;
+            }
+
+            return null;
+        }
+
+        public void UsaBuscaBinaria()
+        {
+            var lista = new List<int> { };
+            for (var i = 1; i <= 100; i++)
+            {
+                lista.Add(i);
+            }
+
+            Console.WriteLine("Digite o número que deseja buscar da lista: ");
+            var item = Convert.ToInt32(Console.ReadLine());
+
+            var listaConcatenada = String.Join(",", lista);
+            Console.WriteLine($"\nLista: [{listaConcatenada}]\n");
+
+            var busca = new BuscaBinaria();
+            var resultado = busca.ImplementaBuscaBinaria(lista, item);
+
+            if (resultado == null)
+            {
+                Console.WriteLine($"\nNúmero {item} não encontrado.");
+                return;
+            }
+
+            Console.WriteLine($"\nNúmero {resultado} encontrado com {busca.ContadorDeTentativas} tentativas.");
+        }
+    }
+}
---
+++ Algoritmos/Algoritmos/OrdenacaoPorSelecao.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmos
+{
+    internal class OrdenacaoPorSelecao
+    {
+
+        private int BuscaIndiceDoMenor(List<int> lista)
+        {
+            var menor = lista[0];
+            var menorIndice = 0;
+            for (int i = 0; i < lista.Count; i++)
+            {
+                if (i < menor) 
+                { 
+                    menor = lista[i]; 
+                    menorIndice = i;
+                }
+            }
+
+            return menorIndice;
+        }
+
+        public List<int> OrdernaPorSelecao(List<int> lista)
+        {
+            var novaLista = new List<int>();
+
+            for(var i = 0; i < lista.Count; i++)
+            {
+                var indiceDoMenor = BuscaIndiceDoMenor(lista);
+                novaLista.Add(indiceDoMenor);
+                lista.RemoveAt(indiceDoMenor);
+            }
+
+            return novaLista;
+        }
+
+        public void InterfaceOrdenarPorSelecao()
+        {
+            var arrayAleatorio = new List<int>() { };
+            var random = new Random();
+
+            for (int i = 0; i < 10; i++)
+                arrayAleatorio.Add(random.Next(1, 101));
+            
+            Console.WriteLine(String.Join(",", arrayAleatorio));
+
+            var ordenacao = new OrdenacaoPorSelecao().OrdernaPorSelecao(arrayAleatorio);
+
+            Console.WriteLine("\n\n" + String.Join(",", ordenacao));
+        }
+    }
+}
